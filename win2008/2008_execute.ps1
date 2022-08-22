29587f1b3952a75fa531132cd287c68b9407e523
vinayaa-airgap <101220134+vinayaa-airgap@users.noreply.github.com>
2022-08-21 19:57:36 -0700

    Update 2008_execute.ps1

--- win2008/2008_execute.ps1
+++ win2008/2008_execute.ps1
@@ -5,7 +5,7 @@ Foreach ($server in $servers)
     #Set-Item WSMan:\localhost\Client\TrustedHosts -Value $server -Force
     Write-Output "`r`nBefore Modifying Subnet"
     Invoke-Command -ComputerName $server -Credential $cred -ScriptBlock {netsh interface ipv4 show config | findstr "Subnet Prefix"}
-    Invoke-Command -ComputerName $server -Credential $cred -InDisconnectedSession -FilePath c:\2008_subnet.ps1
+    Invoke-Command -ComputerName $server -Credential $cred -InDisconnectedSession -FilePath c:\win2008\2008_subnet.ps1
     sleep 5
     Write-Output "`r`nAfter Modifying Subnet"
     Invoke-Command -ComputerName $server -Credential $cred -ScriptBlock {netsh interface ipv4 show config | findstr "Subnet Prefix"}
