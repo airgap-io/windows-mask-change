52449cd7f3462c46a12928d70a5fe34e2f04f511
vinayaa-airgap <101220134+vinayaa-airgap@users.noreply.github.com>
2022-08-21 21:22:38 -0700

    Update 2012_execute.ps1

--- win2012/2012_execute.ps1
+++ win2012/2012_execute.ps1
@@ -2,5 +2,5 @@ $servers = Get-Content servers.txt
 $cred = Get-Credential
 Foreach ($server in $servers)
 {
-    Invoke-Command -ComputerName $server -Credential $cred -FilePath c:\subnet.ps1
+    Invoke-Command -ComputerName $server -Credential $cred -FilePath c:\win2012\2012_subnet.ps1
 }
